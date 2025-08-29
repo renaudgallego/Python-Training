e346b10c9fb015ebb8a622bdddc97b0c89f7519f
Renaud Gallego <renaud.gallego@gmail.com>
2025-08-29 14:16:05 +0200

    clarification regarding Basic 17 + moving Classes.py to the Basics instead of Exervices folder since it is not an exercice

--- Basics/17_Keyword arguments using function.py
+++ Basics/17_Keyword arguments using function.py
@@ -4,7 +4,7 @@ def increment(number, by):
 
 print(increment(2, 1))
 
-# line 6 can be cleary understood by another dev reading the content of the function increment
+# line 5 can be cleary understood by another dev reading the entire content of the function increment
 # to avoid this we can specify 'keyword arguments' like below to ease devs life
 print(increment(number=2, by=1))
 # or
